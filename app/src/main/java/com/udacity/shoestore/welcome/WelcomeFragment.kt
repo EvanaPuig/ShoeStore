30b612d4bee3c72a42899b8f665f96ed67b9949d
Evana Margain Puig <evanamp233@gmail.com>
2021-01-07 17:10:37 -0600

    Step 8

--- app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
@@ -5,21 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+import com.udacity.shoestore.login.LoginFragmentDirections
 
 class WelcomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.instructionsButton.setOnClickListener {
+            navigateToInstructionsFragment(it)
+        }
+
+        return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun navigateToInstructionsFragment(view: View) {
+        val navigationAction = WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment()
+        view.findNavController().navigate(navigationAction)
+    }
 
 }
