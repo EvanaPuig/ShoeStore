d8003f2f623ee45277fc74789c45eabc1f938623
Evana Margain Puig <evanamp233@gmail.com>
2021-01-08 10:47:47 -0600

    Step11

--- app/src/main/java/com/udacity/shoestore/shoeList/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoeList/ShoeListFragment.kt
@@ -17,12 +17,20 @@ class ShoeListFragment : Fragment() {
     ): View {
         _binding = FragmentShoeListBinding.inflate(inflater, container, false)
 
+        binding.shoeListDetailButton.setOnClickListener {
+            navigateToDetailFragment(it)
+        }
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun navigateToDetailFragment(view: View) {
+        // TODO
     }
 
 }
